86ae6edd44d8dcfcd7dc83fee260850ea473db7b
Luomanmäki Alisa <58733101+Sisarus@users.noreply.github.com>
2022-08-05 14:14:33 +0300

    Putting things to order

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -3,13 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeathZone : MonoBehaviour
-{
-    public MainManager Manager;
+public class DeathZone : MonoBehaviour {
+    public GameBehavior gb;
 
-    private void OnCollisionEnter(Collision other)
-    {
-        Destroy(other.gameObject);
-        Manager.GameOver();
+    private void OnCollisionEnter (Collision other) {
+        Destroy (other.gameObject);
+        gb.GameOver ();
     }
 }
